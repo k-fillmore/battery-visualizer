0f6620dbe4fb9ed9de4524d4b78bd328f0a0b64d
Kris <kris.fillmore@gmail.com>
2021-05-15 14:48:17 -0700

    massive add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import BootstrapNavbar from './BootstrapNavbar'
+import InputComponent from './InputComponent'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BootstrapNavbar/>
+    <InputComponent/>
+    </>
+
+
+  )
 }
 
 export default App;
